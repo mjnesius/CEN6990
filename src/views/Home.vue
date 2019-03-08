91d11e37310cc3088fce2bd3d1d5b37a295ece03
sr107 <31510403+sr107@users.noreply.github.com>
2019-03-08 17:26:53 -0600

    Yes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,180 +1,19 @@
 <template>
   <div class="home container">
-    <h1>This is the Home page</h1>
     <div class="jumbotron">
-      <h1 class="display-3">Instructors in the Spotlight</h1>
+      <h1 class="display-3">Home Page</h1>
+      <img
+        class="d-block w-100 img-fluid img-responsive"
+        src="../assets/students.jpg"
+        alt="First slide"
+      />
+      <p class="lead">This is the home page!</p>
       <hr class="my-4" />
-      <div class="card" style="background-color:#009CDE !important">
-        <div class="row  mt-3" style="margin-left:1px; margin-right:1px; ">
-          <div class="col-sm-6">
-            <div class="card mb-3">
-              <div
-                class="row  mt-3"
-                style="margin-left:1px; margin-right:1px; "
-              >
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <div style="height: 190px">
-                      <img
-                        class="img-fluid"
-                        alt="Responsive image"
-                        style="height: auto; width: auto; display: block;"
-                        src="../assets/team_03.jpg"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <h4
-                      class="card-header"
-                      style="background-color:#8DC8E8 !important"
-                    >
-                      Professor Jamie Smith
-                    </h4>
-                    <div class="card-body">
-                      <h5 class="card-title">PhD MIT</h5>
-                      <h6 class="card-subtitle text-muted">Cybersecurity</h6>
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">
-                        I have over 20 years of industry experience and look
-                        forward to sharing my knowledge with you!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="card mb-3">
-              <div
-                class="row  mt-3"
-                style="margin-left:1px; margin-right:1px; "
-              >
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <div style="height: 190px">
-                      <img
-                        class="img-fluid"
-                        alt="Responsive image"
-                        style="height: auto; width: auto; display: block;"
-                        src="../assets/team_02.jpg"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <h4
-                      class="card-header"
-                      style="background-color:#8DC8E8 !important"
-                    >
-                      Professor Ronald Jackson
-                    </h4>
-                    <div class="card-body">
-                      <h5 class="card-title">PhD NYU</h5>
-                      <h6 class="card-subtitle text-muted">Database Systems</h6>
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">
-                        My expertise is in distributed, real-time databases.
-                        Come learn with me!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card mt-3" style="background-color:#fffff !important">
-        <div class="row  mt-3" style="margin-left:1px; margin-right:1px;">
-          <div class="col-sm-6">
-            <div class="card mb-3" style="background-color:#009CDE !important">
-              <div
-                class="row  mt-3"
-                style="margin-left:1px; margin-right:1px; "
-              >
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <h4
-                      class="card-header"
-                      style="background-color:#B6ADA5 !important"
-                    >
-                      Professor Raul Gomez
-                    </h4>
-                    <div class="card-body">
-                      <h5 class="card-title">PhD Standford</h5>
-                      <h6 class="card-subtitle text-muted">
-                        Software Engineering
-                      </h6>
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">
-                        I have over 15 years developing software in the defense
-                        industry!
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <div style="height: 190px">
-                      <img
-                        style="height: auto; width: auto; display: block;"
-                        src="../assets/team_01.jpg"
-                        class="img-fluid"
-                        alt="Responsive image"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="card mb-3" style="background-color:#009CDE !important">
-              <div
-                class="row  mt-3"
-                style="margin-left:1px; margin-right:1px; "
-              >
-                <div class="col-sm-6">
-                  <div class="card mb-3">
-                    <h4
-                      class="card-header"
-                      style="background-color:#B6ADA5 !important"
-                    >
-                      Professor Gabe McMasters
-                    </h4>
-                    <div class="card-body">
-                      <h5 class="card-title">PhD Columbia University</h5>
-                      <h6 class="card-subtitle text-muted">UX Design</h6>
-                    </div>
-                    <div class="card-body">
-                      <p class="card-text">. Come learn with me!</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="card mh-100">
-                    <div style="height: 190px">
-                      <img
-                        class="img-fluid"
-                        alt="Responsive image"
-                        style="height: auto; width: auto; display: block;"
-                        src="../assets/testimonials-2.jpg"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+        consequatur aliquam saepe qui. Dolor rem corporis pariatur deleniti
+        accusamus quos!
+      </p>
     </div>
     <hr />
     <div class="Home container2">
@@ -212,42 +51,48 @@
 
     <br />
 
-    <div class="Home container3">
-      <h5 class="card-header">New Courses</h5>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 1</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <a href="#" class="btn btn-primary">Go to Topic 1</a>
+    <div class="jumbotron">
+      <h1 class="display-3">New Courses!</h1>
+      <div class="Home container3">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <img src="../assets/bubbles.jpg" class="img-fluid" />
+              <div class="card-body">
+                <h5 class="card-title">Course 1</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <a href="#" class="btn btn-primary">Go to Topic 1</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 2</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <a href="#" class="btn btn-primary">Go to Topic 2</a>
+          <div class="col-md-3">
+            <div class="card">
+              <img src="../assets/bubbles.jpg" class="img-fluid" />
+              <div class="card-body">
+                <h5 class="card-title">Course 2</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <a href="#" class="btn btn-primary">Go to Topic 2</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 3</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <button class="btn btn-link">Link</button>
+          <div class="col-md-3">
+            <div class="card">
+              <img src="../assets/bubbles.jpg" class="img-fluid" />
+              <div class="card-body">
+                <h5 class="card-title">Course 3</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <button class="btn btn-link">Link</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 2</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <button class="btn btn-link">Link</button>
+          <div class="col-md-3">
+            <div class="card">
+              <img src="../assets/bubbles.jpg" class="img-fluid" />
+              <div class="card-body">
+                <h5 class="card-title">Course 2</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <button class="btn btn-link">Link</button>
+              </div>
             </div>
           </div>
         </div>
@@ -257,41 +102,43 @@
     <br />
 
     <div class="Home container4">
-      <h5 class="card-header">Trending Courses</h5>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 1</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <a href="#" class="btn btn-primary">Go to Topic 1</a>
+      <div class="jumbotron">
+        <h1 class="display-3">Trending Courses!</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Course 1</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <a href="#" class="btn btn-primary">Go to Topic 1</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 2</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <a href="#" class="btn btn-primary">Go to Topic 2</a>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Course 2</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <a href="#" class="btn btn-primary">Go to Topic 2</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 3</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <button class="btn btn-link">Link</button>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Course 3</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <button class="btn btn-link">Link</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Course 4</h5>
-              <p class="card-text">Supporting text for topic description.</p>
-              <button class="btn btn-link">Link</button>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Course 4</h5>
+                <p class="card-text">Supporting text for topic description.</p>
+                <button class="btn btn-link">Link</button>
+              </div>
             </div>
           </div>
         </div>
@@ -307,4 +154,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.jumbotron1 {
+  background-image: url(../assets/areyourready.jpg);
+  background-size: cover;
+}
+</style>
