d740d651c5dc9126891e8ae10ad3d7b66f4a3705
Mike Nesius <mn66@students.uwf.edu>
2019-03-08 19:49:28 -0500

    adding effects to the testimonials carousel

--- src/views/About.vue
+++ src/views/About.vue
@@ -178,30 +178,49 @@
         </div>
       </div>
     </div>
-    <div class="jumbotron test mt-5 mb-5">
-      <h3 class="display-4">Testimonials</h3>
+    <div class="jumbotron mt-5 mb-5">
+      <div class="jumbotron-inside">
+        <h3
+          class="display-4"
+          style="color: white; text-shadow: 0 3.5px 3.5px rgba(0, 0, 0, 0.6);"
+        >
+          Testimonials
+        </h3>
+      </div>
       <div
         id="inspirationCarousel"
-        class="carousel slide"
+        class="carousel2 slide mb-1 mt-1"
         data-ride="carousel"
-        data-interval="5000"
+        data-interval="6000"
+        style="max-height: 550px; !important"
       >
-        <div class="carousel-inner ">
+        <div class="carousel2-inner">
           <div
-            class="carousel-item item"
+            class="carousel-item item "
             v-for="(test, idx) in testimonials"
             :key="test.id"
             :class="{ active: idx == 0 }"
             width="auto"
           >
             <img
-              class="d-block w-100 img-fluid img-responsive"
+              class="d-block h-100 img-fluid img-responsive fill"
               :src="test.img"
               :alt="test.name"
               z-index="24"
+              style=" box-shadow: 0px 0px 10px 5px rgba(255, 184, 28, .4);border-radius: 25% 10%; !important;"
             />
-            <div class="carousel-content">
-              <h2 class="caro-h2">{{ test.blurb }}</h2>
+            <!--<div class="carousel2-content" style="background-color: #003865; opacity:0.9; min-height:100px; display: flex; text-align: center; justify-content: center; 
+                    align-items: center;">
+                <h2 class="caro-h2" >{{ test.blurb }}</h2>
+              </div>-->
+
+            <div
+              class=" carousel-caption d-none d-md-block rounded border-left"
+              style="box-sizing: border-box;padding: 15px; max-width: 550px; background-color: rgba(0, 76, 151, 0.7);!important"
+            >
+              <h4>{{ test.blurb }}</h4>
+              <h5>-{{ test.name }}</h5>
+              <h6>{{ test.specialty }}</h6>
             </div>
           </div>
         </div>
@@ -265,7 +284,7 @@ export default {
         {
           id: 0,
           name: "Joselle",
-          specialty: "Cybersecurity",
+          specialty: "Cybersecurity 2019",
           blurb:
             "Music wasn't paying the bills, so I skilled up and got an entry level developer position with a tier-1 defense contactor. Thanks Empower!",
           img: require("../assets/testimonials-6.jpg")
@@ -273,7 +292,7 @@ export default {
         {
           id: 1,
           name: "Joe",
-          specialty: "Database Systems",
+          specialty: "Database Systems 2019",
           blurb:
             "UWF Empower helped me kickstart my career change. With the mobile app development skills I learned, I was able to launch my Hat Hailing service and we just reached Unicorn status!",
           img: require("../assets/testimonials-4.jpg")
@@ -281,15 +300,15 @@ export default {
         {
           id: 2,
           name: "DJ Sneezey",
-          specialty: "Software engineering",
+          specialty: "Software engineering 2019",
           blurb:
             "I wrote my on mixing software. Now, no one can copy my sound!",
           img: require("../assets/security.jpg")
         },
         {
           id: 3,
-          name: "@guiGuy",
-          specialty: "UX Design",
+          name: "@guiGal",
+          specialty: "UX Design 2019",
           blurb: "Empower gave me the skills I needed, on my schedule!",
           img: require("../assets/testimonials-3.jpg")
         }
@@ -300,11 +319,58 @@ export default {
 </script>
 
 <style scoped>
+.jumbotron {
+  position: relative;
+}
+.jumbotron::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("../assets/making_waves_image.jpg");
+  background-size: cover;
+  filter: sepia(100%) hue-rotate(190deg) saturate(500%) blur(3px) opacity(0.6)
+    grayscale(0.2);
+}
+.jumbotron-inside {
+  /* This will make it stack on top of the ::before */
+  position: relative;
+}
+.carousel-caption {
+  left: 43%;
+  bottom: 35%;
+  transform: translateX(10%);
+  transform: translatey(-80%);
+  color: white;
+  text-shadow: 0 3px 3.5px rgba(0, 0, 0, 0.8);
+}
 .carousel {
   max-height: 370px;
   min-height: 250px;
   overflow: hidden;
 }
+.carousel2,
+.item,
+.active {
+  height: 50%;
+  overflow: hidden;
+}
+.carousel2-inner {
+  height: 50%;
+  overflow: hidden;
+}
+.fill {
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  background-size: cover;
+  -o-background-size: cover;
+}
+
 .item img {
   width: auto;
   height: auto;
@@ -338,8 +404,10 @@ export default {
 .carousel2-content {
   position: absolute;
   font-weight: bolder;
-  top: 5%;
+  top: 0%;
   left: 5%;
+  right: 5%;
+  margin: 0 auto;
   z-index: 20;
   color: white;
   text-shadow: 0 3.5px 3.5px rgba(0, 0, 0, 0.6);
