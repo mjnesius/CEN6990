155fce8d74fb416f1e7507360a83c786727919e7
Stephan Urbanczyk <smu1@students.uwf.edu>
2019-03-10 17:12:29 -0500

    fixed dynamic loading of svg for topic page

--- src/views/Topic.vue
+++ src/views/Topic.vue
@@ -3,7 +3,9 @@
     <div class="h-100">
       <div class="row p-5 h-100 justify-content-center align-items-center">
         <div class="col-md-6">
-          <img src="../assets/undraw_programmer_imem.svg" alt width="400">
+          <img v-if="topic == 'Software Development'" src="../assets/undraw_programmer_imem.svg" alt width="400">
+          <img v-else-if="topic == 'Information Technology'" src="../assets/undraw_dashboard_nklg.svg" alt width="400">
+          <img v-else src="../assets/undraw_security_o890.svg" alt width="400">
         </div>
         <div class="col-md-6">
           <h1 class="dispay-3">{{topic}}</h1>
