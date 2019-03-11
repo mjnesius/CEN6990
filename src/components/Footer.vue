fb8b506a9ef2a52de22b58a3d3ebb0f97836452c
Stephan Urbanczyk <smu1@students.uwf.edu>
2019-03-11 00:29:12 -0500

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-primary">
     <div class="container">
-      <div class="row">
-        <div class="col text-center py-2 text-white">
-          <img src="../assets/uwf logo2.png" alt="" width="450" height="80" />
-          <h4>UWF Empowers</h4>
-          <p>Copyright &copy; 2019</p>
+      <div class="row ">
+        <div class="col d-flex justify-content-center align-items-center py-2 text-white">
+          <img src="../assets/uwf logo2.png" alt="" height="60" />
+          <h4 class="lead">Copyright &copy; 2019</h4>
+          <h4 class="ml-5 lead">UWF Empowers</h4>
         </div>
       </div>
     </div>
