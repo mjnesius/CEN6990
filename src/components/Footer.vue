148baee950cac745731c6f07d6a2d21637c06bd9
Stephan Urbanczyk <smu1@students.uwf.edu>
2019-03-11 01:46:49 -0500

    more fixes to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-primary">
     <div class="container">
-      <div class="row ">
-        <div class="col d-flex justify-content-center align-items-center py-2 text-white">
-          <img src="../assets/uwf logo2.png" alt="" height="60" />
-          <h4 class="lead">Copyright &copy; 2019</h4>
-          <h4 class="ml-5 lead">UWF Empowers</h4>
+      <div class="row">
+        <div class="col d-flex justify-content-around align-items-center py-2 text-white">
+          <img src="../assets/uwf logo2.png" alt height="60">
+          <h4 class="lead text-center mt-2 text-white">Copyright &copy; 2019</h4>
+          <h4 class="lead ml-5 mt-2">UWF Empowers</h4>
         </div>
       </div>
     </div>
