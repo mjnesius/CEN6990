abfedc09a63c0fd2aa774d7a5183ccb109356cae
Stephan Urbanczyk <smu1@students.uwf.edu>
2019-03-12 17:26:11 -0500

    footer fix for responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col d-flex justify-content-around align-items-center py-2 text-white">
-          <img src="../assets/uwf logo2.png" alt height="60">
+          <img class="d-none d-md-inline-block" src="../assets/uwf logo2.png" alt height="60">
           <h4 class="lead text-center mt-2 text-white">Copyright &copy; 2019</h4>
           <h4 class="ml-5 mt-2">UWF Empowers</h4>
         </div>
