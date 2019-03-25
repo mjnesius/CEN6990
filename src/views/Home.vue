121d4a0f95131d7686e697f97728c96f40ace6f2
sr107 <31510403+sr107@users.noreply.github.com>
2019-03-25 18:37:24 -0500

    UX design chnages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,195 +5,93 @@
         <section class="dark-overlay">
           <h1 class="display-4">Get Ready to Learn!</h1>
           <p>Thousands of classes to fuel your creativity and career.</p>
-          <!-- <figure> -->
-          <!-- <button type="button" href="About">Learn More</button> -->
           <a
             class="button btn-light btn-lg btn-primary"
             href="About"
             role="button"
-            >Learn More</a
           >
-
-          <!-- </figure> -->
+            Learn More
+          </a>
         </section>
       </div>
     </section>
     <hr />
+
     <div class="Container mt-5">
       <h2 class="mb-5 text-left">Trending Courses</h2>
-      <div class="card-deck">
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/adults-chairs-class-515166.jpg"
-              class="img-fluid"
-              alt="Area for Topic1"
-            />
-            <h3 class="card-title mt-2">Computer Network</h3>
-            <p class="card-text mt-3">
+      <div class="card-deck1 m-1">
+        <div class="card">
+          <img
+            src="../assets/adults-chairs-class-515166.jpg"
+            class="card-img-top"
+            alt="Trending Course1"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Computer Network</h5>
+            <p class="card-text">
               System for connecting hardware, software, resource sharing and
               information transmission of network with fully functional network
               software by using communication equipment and lines to connect
               multiple computer systems with different geographical locations
               and functions independently.
             </p>
-            <a href="#" class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
-            >
+            <a href="#" class="btn btn-primary btn mt-auto">Find A Course</a>
           </div>
         </div>
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/employerShowcase.jpg"
-              class="img-fluid"
-              alt="Area for Topic2"
-            />
-            <h3 class="card-title mt-2">Computer Composition</h3>
-            <p class="card-text mt-3">
+        <div class="card">
+          <img
+            src="../assets/employerShowcase.jpg"
+            class="card-img-top"
+            alt="Trending Course2"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Computer Composition</h5>
+            <p class="card-text">
               Computer composition refers to the logical realization of the
               system structure, including the composition of the data flow and
               control flow within the machine and the logic design.
             </p>
-            <a href="#" class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
-            >
+            <a href="#" class="btn btn-primary btn mt-auto">Find A Course</a>
           </div>
         </div>
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/boss-brainstorming-business-1093913.jpg"
-              class="img-fluid"
-              alt="Area for Topic3"
-            />
-            <h3 class="card-title mt-2">Advanced programming language</h3>
-            <p class="card-text mt-3">
+        <div class="card">
+          <img
+            src="../assets/boss-brainstorming-business-1093913.jpg"
+            class="card-img-top"
+            alt="Trending Course3"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Advanced programming language</h5>
+            <p class="card-text">
               A programming language is a language used to write computer
               programs. The basis of a language is a set of tokens and a set of
               rules. The whole of the token string consisting of tokens
               according to the rules is the language.
             </p>
-            <a href="#" class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
-            >
+            <a href="#" class="btn btn-primary btn mt-auto">Find A Course</a>
           </div>
         </div>
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/se.jpg"
-              class="img-fluid"
-              alt="Area for Topic3"
-            />
-            <h3 class="card-title mt-2">Computer Graphics</h3>
-            <p class="card-text mt-3">
+        <div class="card">
+          <img
+            src="../assets/se.jpg"
+            class="card-img-top"
+            alt="Trending Course4"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Computer Graphics</h5>
+            <p class="card-text">
               The main research content of computer graphics is to study how to
               represent graphics in computers and the related principles and
               algorithms for computing, processing and displaying graphics using
               computers.
             </p>
-            <a href="#" class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
-            >
+            <a href="#" class="btn btn-primary btn mt-auto">Find A Course</a>
           </div>
         </div>
       </div>
     </div>
+    <hr />
 
-    <!--<div class="Container mt-5">
-      <h2 class="mb-5 text-left">Trending Courses</h2>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card-deck">
-              <div class="card mb-3">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img
-                      src="../assets/inspiring-2.jpg"
-                      class="card-img"
-                      alt="..."
-                    />
-                  </div>
-                <h5 class="card-title">Computer Network</h5>
-                <p class="card-text"> System for connecting hardware, 
-                       software and resource sharing and information
-                        transmission of network with fully functional 
-                        network software by using communication equipment
-                         and lines to connect multiple computer systems with
-                          different geographical locations and functions independently</p>
-                <a href="#" class="btn btn-dark btn-sm float-right"
-                  >Learn more..</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-           <div class="card-deck">
-              <div class="card mb-3">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img
-                      src="../assets/inspiring-2.jpg"
-                      class="card-img"
-                      alt="..."
-                    />
-                  </div>
-                <h5 class="card-title">Computer Composition</h5>
-                <p class="card-text"> Computer composition refers to the logical realization of 
-                        the system structure, including the composition of the data 
-                        flow and control flow within the machine and the logic design.</p>
-                <a href="#" class="btn btn-dark btn-sm float-right"
-                  >Learn more..</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-           <div class="card-deck">
-              <div class="card mb-3">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img
-                      src="../assets/inspiring-2.jpg"
-                      class="card-img"
-                      alt="..."
-                    />
-                  </div>
-                <h5 class="card-title">Advanced programming language</h5>
-                <p class="card-text">A programming language is a language used to write 
-                        computer programs. The basis of a language is a set 
-                        of tokens and a set of rules. The whole of the token 
-                        string consisting of tokens according to the rules is the language.</p>
-                <a href="#" class="btn btn-dark btn-sm float-right">Learn more..</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-           <div class="card-deck">
-              <div class="card mb-3">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img
-                      src="../assets/inspiring-2.jpg"
-                      class="card-img"
-                      alt="..."
-                    />
-                  </div>
-                <h5 class="card-title">Computer Graphics</h5>
-                <p class="card-text">The main research content of computer 
-                  graphics is to study how to represent graphics in computers
-                   and the related principles and algorithms for computing, 
-                   processing and displaying graphics using computers.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>-->
     <br />
     <br />
     <div class="container">
@@ -204,7 +102,7 @@
         data-ride="carousel"
         data-interval="false"
       >
-        <div class="carousel-inner bg-primary">
+        <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="card-deck m-1">
               <div class="card">
@@ -451,19 +349,20 @@
     </div>
     <hr />
     <br />
+
     <div class="Container mt-5">
       <h2 class="mb-5 text-left">Browse By Topic</h2>
-      <div class="card-deck">
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/employer3.jpg"
-              class="img-fluid"
-              alt="Area for Topic1"
-            />
-            <h3 class="card-title mt-2">Software Development</h3>
-            <p class="card-text mt-3">
-              >Web design encompasses many different skills and disciplines in
+      <div class="card-deck m-1">
+        <div class="card">
+          <img
+            src="../assets/employer3.jpg"
+            class="card-img-top"
+            alt="Area for Topic1"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Software Development</h5>
+            <p class="card-text">
+              Web design encompasses many different skills and disciplines in
               the production and maintenance of websites. The different areas of
               web design include web graphic design; interface design;
               authoring, including standardised code and proprietary software;
@@ -474,20 +373,20 @@
             <a
               href="/topic/0"
               alt="Development"
-              class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
+              class="btn btn-primary btn mt-auto"
+              >Start Course</a
             >
           </div>
         </div>
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/workplace.jpg"
-              class="img-fluid"
-              alt="Area for Topic2"
-            />
-            <h3 class="card-title mt-2">Information Technology</h3>
-            <p class="card-text mt-3">
+        <div class="card">
+          <img
+            src="../assets/workplace.jpg"
+            class="card-img-top"
+            alt="Area for Topic2"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">Information Technology</h5>
+            <p class="card-text">
               A network administrator is the person designated in an
               organization whose responsibility includes maintaining computer
               infrastructures with emphasis on networking. Responsibilities may
@@ -495,23 +394,20 @@
               interactions as well as network integrity/resilience are the key
               areas of focus.
             </p>
-            <a
-              href="/topic/1"
-              alt="IT"
-              class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
+            <a href="/topic/1" alt="IT" class="btn btn-primary btn mt-auto"
+              >Start Course</a
             >
           </div>
         </div>
-        <div class="card cardSet1 text">
-          <div class="card-body">
-            <img
-              src="../assets/adult-aged-blur-834863.jpg"
-              class="img-fluid"
-              alt="Area for Topic3"
-            />
-            <h3 class="card-title mt-2">CyberSecurity</h3>
-            <p class="card-text mt-3">
+        <div class="card">
+          <img
+            src="../assets/adult-aged-blur-834863.jpg"
+            class="card-img-top"
+            alt="Area for Topic3"
+          />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">CyberSecurity</h5>
+            <p class="card-text">
               Computer security, cybersecurity[1] or information technology
               security (IT security) is the protection of computer systems from
               theft or damage to their hardware, software or electronic data, as
@@ -521,8 +417,8 @@
             <a
               href="/topic/2"
               alt="CyberSecurity"
-              class="btn btn-primary btn-sm float-right"
-              >Find A Course</a
+              class="btn btn-primary btn mt-auto"
+              >Start Course</a
             >
           </div>
         </div>
@@ -569,9 +465,17 @@ tbody tr {
   position: relative;
 }
 
-/* button.btn {
-  margin-top: auto;
+/* .carousel-inner {
+  background-color : #669999;
+}
+
+.card2 {
+  background-color : #cc6666;
 } */
+
+.carousel-inner {
+  background-color: #cc6666;
+}
 
 .card:hover {
   -webkit-box-shadow: -1px 9px 40px -12px rgba(0, 0, 0, 0.75);
@@ -639,9 +543,10 @@ button:hover {
   width: 4%;
 }
 
-.card-deck {
+.card-deck1 {
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-gap: 0.5rem;
+  background-color: #669999;
 }
 </style>
