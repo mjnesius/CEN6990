d779adc38eb176dd4366912e7f078f6cfb64bd1c
Stephan Urbanczyk <smu1@students.uwf.edu>
2019-04-15 17:10:43 -0500

    deleted un-needed comments

--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -1,15 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
 Cypress.Commands.add("loginUser", () => {
   cy.visit("/");
   cy.get(":nth-child(3) > .nav-link").click();
@@ -29,18 +17,6 @@ Cypress.Commands.add("logoutInstructor", () => {
   cy.url().should("include", "login");
 });
 Cypress.Commands.add("logoutUser", () => {
-  cy.get(':nth-child(3) > .nav-link').click();
+  cy.get(":nth-child(3) > .nav-link").click();
   cy.url().should("include", "login");
 });
-//
-//cy.get(':nth-child(3) > .nav-link')
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
